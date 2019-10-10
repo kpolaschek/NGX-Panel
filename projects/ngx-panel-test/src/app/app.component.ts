0cf76a9e170855493e7d61d4d5c523f66f872074
kpolaschek <kpolaschek@inconso.de>
2019-10-10 16:31:36 +0200

    Included library module to the test application, changed selector name of the panel component and prepared the test application

--- projects/ngx-panel-test/src/app/app.component.ts
+++ projects/ngx-panel-test/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-panel-test';
+  title = 'NGX-Panel test application';
 }
--- projects/ngx-panel-test/src/app/app.module.ts
+++ projects/ngx-panel-test/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxPanelModule } from 'ngx-panel';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
